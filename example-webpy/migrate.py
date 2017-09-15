ebde412e9d9a3b2c757b1c8acf0b3cc8fde921b3
Krzysztof Szumny <noisy.pl@gmail.com>
2017-09-15 12:18:57 +0200

    not example-webpy, example-cherrypy in mvp

--- example-webpy/migrate.py
+++
@@ -1,9 +0,0 @@
-from social_webpy.models import SocialBase
-
-from app import engine
-from models import Base
-
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
-    SocialBase.metadata.create_all(engine)
